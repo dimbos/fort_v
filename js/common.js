fc022f9cabb2e81e906437906b189f60bb440541
dimbos <dimboss@list.ru>
2017-06-13 18:37:41 +0300

    aside

--- js/common.js
+++ js/common.js
@@ -1,5 +1,11 @@
 $(function() {
 
+	$(".box_mnu .panel-heading").each(function(){
+		var ph = $(this).height() + 3;
+		var pdt = $(this).find(".dropdown-toggle");
+		pdt.height(ph);
+	});
+
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
